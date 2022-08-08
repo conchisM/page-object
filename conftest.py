5245a2ba0fdb8b12eb59006d4359468f57cdc3e0
Kostya Antonov <kostya_antonov_04@mail.ru>
2022-08-08 20:40:13 +0500

    fixed conftest.py, changed test_main_page.py

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from selenium import webdriver
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
+    parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
 
 
--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,11 @@
-import pytest
-from selenium import webdriver
+from .pages.main_page import MainPage
+
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 
 class TestMainPage:
-    def test_guest_should_see_login_link(self, browser): #fixtures are described in the file conftest.py
-        browser.get(link)
-        browser.find_element_by_css_selector("#login_link")
-
-    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
-        browser.get(link)
-        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+        page.open_page()  # открываем страницу
+        page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
