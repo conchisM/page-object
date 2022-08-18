2e859fe767f39d94360000a8557c608625ae185e
Kostya Antonov <kostya_antonov_04@mail.ru>
2022-08-18 23:30:37 +0500

    added parametrize

--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,13 @@
 from .pages.product_page import ProductPage
+import pytest
 
 
-class TestProductPage:
-    def test_guest_can_go_to_product_page(self, browser):
-        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
-        page = ProductPage(browser, link)
-        page.open_page()
-        page.add_to_basket()
-        page.solve_quiz_and_get_code()
-        page.should_name_egual()
+@pytest.mark.parametrize('num', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks = pytest.mark.xfail), 8, 9])
+def test_guest_can_go_to_product_page(browser, num):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}'
+    page = ProductPage(browser, link)
+    page.open_page()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_name_egual()
 
