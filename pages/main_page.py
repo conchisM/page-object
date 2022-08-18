1c1b00aeed04c2389b7af1b0ca1fd89597b2f30d
Kostya Antonov <kostya_antonov_04@mail.ru>
2022-08-18 15:43:27 +0500

    added processing alert

--- pages/main_page.py
+++ pages/main_page.py
@@ -8,6 +8,8 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
         return LoginPage(browser=self.browser, url=self.browser.current_url) 
 
     def should_be_login_link(self):
